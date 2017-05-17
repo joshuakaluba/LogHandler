4b1f6d63dbae91c664f3e7b6fc5593db56a2ad03
Joshua Kaluba <joshkaluba@gmail.com>
2017-05-16 22:23:17 -0400

    created test solution

---
+++ Logger.Test/LogEntryTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Logger.Test
+{
+	[TestClass]
+	public class LogEntryTests
+	{
+		[TestMethod]
+		[ExpectedException ( typeof ( ArgumentException ) )]
+		public void LogTestEmptyFileName ( )
+		{
+			LogEntry.Log ( String.Empty , String.Empty );
+			Assert.Fail ( );
+		}
+
+		[TestMethod]
+		[ExpectedException ( typeof ( ArgumentException ) )]
+		public void LogTestInvalidCharacters ( )
+		{
+			LogEntry.Log ( "#@&*!:" , "message content" );
+			Assert.Fail ( );
+		}
+
+		[TestMethod]
+		[ExpectedException ( typeof ( ArgumentException ) )]
+		public void LogErrorEmptyString ( )
+		{
+			LogEntry.LogError ( String.Empty );
+			Assert.Fail ( );
+		}
+
+		[TestMethod]
+		public void LogExceptionErrorTest ( )
+		{
+			try
+			{
+				int denominator = 0;
+
+				int numerator = 12;
+
+				//dividing by zero, this will fail and an exception will be thrown
+				int operation = numerator / denominator;
+			}
+			catch ( DivideByZeroException ex )
+			{
+				LogEntry.LogExceptionError ( ex );
+			}
+		}
+
+		[TestMethod]
+		public void LogMessageTest ( )
+		{
+			LogEntry.Log ( "Transactions" , "The document was successfully saved" );
+		}
+	}
+}
---
+++ Logger.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ( "Logger.Test" )]
+[assembly: AssemblyDescription ( "" )]
+[assembly: AssemblyConfiguration ( "" )]
+[assembly: AssemblyCompany ( "" )]
+[assembly: AssemblyProduct ( "Logger.Test" )]
+[assembly: AssemblyCopyright ( "Copyright ©  2017" )]
+[assembly: AssemblyTrademark ( "" )]
+[assembly: AssemblyCulture ( "" )]
+
+[assembly: ComVisible ( false )]
+
+[assembly: Guid ( "bd133369-a6e8-4b5a-aa7b-7a4833d64774" )]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion ( "1.0.0.0" )]
+[assembly: AssemblyFileVersion ( "1.0.0.0" )]
